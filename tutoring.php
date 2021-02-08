cd7edb2b0055a0e41fa60193b365c615fc2d8523
Joe Moore <60425398+joverandout@users.noreply.github.com>
2021-02-08 13:07:22 +0000

    security vulnerability from axios updated

--- tutoring.php
+++ tutoring.php
@@ -31,11 +31,11 @@
         <div class="inner">
           <h3 class="masthead-brand">Joe</h3>
           <nav class="nav nav-masthead justify-content-center">
-            <a class="nav-link" href="home.html">Home</a>
-            <a class="nav-link" href="about.html">About</a>
-            <a class="nav-link" href="projects.html">Projects</a>
+            <a class="nav-link" href="home.php">Home</a>
+            <a class="nav-link" href="about.php">About</a>
+            <a class="nav-link" href="projects.php">Projects</a>
             <a class="nav-link active" href="#">Tutoring</a>
-            <a class="nav-link" href="contact.html">Contact</a>
+            <a class="nav-link" href="contact.php">Contact</a>
           </nav>
         </div>
       </header>
